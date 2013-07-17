dfaff99d1ab98bf6ab541e4f0f24a02ef07aa203
bsmt <bsmt@krax.in>
2013-07-16 23:23:09 -0600

    Absolutely no reason that needed to be in a class

--- rectangled/datehelp.py
+++ rectangled/datehelp.py
@@ -1,10 +1,29 @@
 from datetime import datetime, timedelta
 
 
+def find_origin(self, date):
+    '''Find the origin sunday given a day in the current week'''
+
+    last_sunday = (date - timedelta(days=date.weekday()) + 
+                   timedelta(days=6, weeks=-1))
+    origin_sunday = last_sunday + timedelta(weeks=-51)
+    return origin_sunday
+
+
+def find_end(self, date):
+    '''Find the ending saturday relative to date'''
+
+    if date is None:
+       date = datetime.now()
+    next_saturday = (date - timedelta(days=date.weekday()) +
+                     timedelta(days=5))
+    return next_saturday
+
+
 def dates_for_column(week):
     '''week: a value from 0 to 51, relative to the origin date'''
 
-    origin = Datehelper.find_origin(datetime.now())
+    origin = find_origin(datetime.now())
     week_start = origin + timedelta(weeks=week)  # find the starting sunday
     
     # needs to be cleaner, oh well
@@ -15,28 +34,3 @@ def dates_for_column(week):
         i += 1
 
     return dates
-
-
-class Datehelper(object):
-    @classmethod
-    def find_origin(self, date):
-        '''Find the origin sunday given a day in the current week'''
-        
-        last_sunday = (date - timedelta(days=date.weekday()) + 
-                       timedelta(days=6, weeks=-1))
-        origin_sunday = last_sunday + timedelta(weeks=-51)
-        return origin_sunday
-
-    @classmethod
-    def find_end(self, date):
-        '''Find the ending saturday relative to date'''
-
-        if date is None:
-           date = datetime.now()
-        next_saturday = (date - timedelta(days=date.weekday()) +
-                         timedelta(days=5))
-        return next_saturday
-        
-    def __init__(self):
-        self.origin = Datehelper.find_origin(datetime.now())
-        self.end = Datehelper.find_end(datetime.now())
